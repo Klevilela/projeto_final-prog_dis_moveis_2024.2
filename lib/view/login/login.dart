3570dd69d0fbec333655fbd5edbfcd02e8d0c32b
Kleberson <klebisvillela@outlook.com>
2025-01-28 14:24:57 -0300

    começando a fazer a tela de login

--- lib/view/login/login.dart
+++ lib/view/login/login.dart
@@ -11,9 +11,66 @@ class Login extends StatefulWidget {
   State<Login> createState() => _LoginState();
 }
 
-class _LoginState extends State<Login> {
+class _LoginState extends State<Login> with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  final TextEditingController _nomeController = TextEditingController();
+  final TextEditingController _senhaController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Quanto Custa - THE'),
+          backgroundColor: Colors.green,
+          foregroundColor: Colors.white,
+          bottom: TabBar(
+              controller: _tabController,
+              tabs: const <Widget>[Text('Login'), Text('Registre-se')]),
+        ),
+        body: TabBarView(
+          controller: _tabController,
+          children: <Widget>[
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  TextField(
+                    controller: _nomeController,
+                    keyboardType: TextInputType.name,
+                    decoration: const InputDecoration(
+                      labelText: 'Nome',
+                    ),
+                  ),
+                  TextField(
+                    controller: _senhaController,
+                    keyboardType: TextInputType.text,
+                    decoration: const InputDecoration(
+                      labelText: 'Senha',
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 40, vertical: 15),
+                        backgroundColor: Colors.blue),
+                    child: const Text('Login'),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+            const Center(child:  Text('oi'))
+          ],
+        ));
   }
 }
